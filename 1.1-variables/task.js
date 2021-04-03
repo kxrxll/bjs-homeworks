71f880351c360a81ebc0eaccf102e489e7a03198
kxrxll <kovalyovkirill@gmail.com>
2021-04-03 17:07:47 +0300

    Tasks solved

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -1,27 +1,24 @@
- /*
-	Кратко о работе с функциями:
-	1) Значения в скобках - это аргументы, которые мы получаем в момент вызова функции. Их и надо присваивать требуемым в заданиях переменным.
-	2) После ключевого слова return вместо комментария необходимо написать переменную либо выражение с ответом.
-	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
- */
-
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let algebra;
+	let geography;
+	let physics;
+	algebra = a;
+	geography = g;
+	physics = p;
+	const averageEstimation = (algebra + geography + physics)/3;
+	return averageEstimation;
 }
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	const myName = userName;
+    const message = `Привет, мир! Меня зовут ${myName}`
+	return message;
 }
 
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+	let y = 22;
+	let z = 0;
+	let result = x * y + 5 * z + x - 1;
+    return result;
 }
