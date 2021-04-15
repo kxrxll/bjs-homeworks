982ad8b14548ab8219879a740bdb4dd1ca249fca
kxrxll <kovalyovkirill@gmail.com>
2021-04-16 00:10:28 +0300

    Task done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,34 @@
-//String.prototype.isPalindrome - для задачи №1
-
-function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+String.prototype.isPalindrome = function () {
+    let original = this.split("").filter(letter => letter != " ").join("").toLowerCase();
+    let reverse = original.split("").filter(letter => letter != " ").reverse().join("").toLowerCase();
+    if (original === reverse) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+function getAverageMark(marks) {
+    if (marks.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let i = 0 ; i < marks.length ; i++) {
+       sum += marks[i];
+    }
+    let average = sum / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
+}
+
+function checkBirthday(date) {
+    let verdict = false;
+    const now = Date.now();
+    const birthday = Date.parse(date);
+    const diff = now - birthday;
+    const age = diff / 31557600000;
+    if (age > 18) {
+        verdict = true;
+    }
+    return verdict;
 }
