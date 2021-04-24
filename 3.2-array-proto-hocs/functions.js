45c9855b53e1a645662c1b6ab63d62fe1f3504d4
kxrxll <kovalyovkirill@gmail.com>
2021-04-24 16:30:44 +0300

    Additions made

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -1,59 +1,30 @@
-const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
+const weapons = [new Knife (), new Staff (), new Axe (), new StormStaff (), new LongBow (), new Bow ()];
 
-function getNames( array ) {
-    const newArray = array.map( function ( item ) {
-        return item.name;
-    } );
-    return newArray;
+getNames = () => {
+    return weapons.map(item => {return item.name});
 }
 
-function getCountReliableWeapons( array, requiredDurability ) {
-    const weaponsArray = array.filter( function ( item ) {
-        if ( item.durability > requiredDurability ) {
-            return item;
-        }
-    } );
-    return weaponsArray.length;
+getCountReliableWeapons = (requiredDurability) => {
+    return weapons.filter(item => {return item.durability > requiredDurability}).length;
 }
 
-function hasReliableWeapons( array, requiredDurability ) {
-    if ( array.find( function ( item ) {
-        return item.durability > requiredDurability;
-    } ) === undefined ) {
-        return false
-    } else {
-        return true
-    }
+hasReliableWeapons = (requiredDurability) => {
+    return weapons.some(item => {return item.durability > requiredDurability});
 }
 
-function getReliableWeaponsNames( array, requiredDurability ) {
-    const weaponsArray = array.filter( function ( item ) {
-        if ( item.durability > requiredDurability ) {
-            return item;
-        }
-    } );
-    const newArray = weaponsArray.map( function ( item ) {
-        return item.name;
-    } );
-    return newArray;
+getReliableWeaponsNames = (requiredDurability) => {
+    return weapons.filter(item => {return item.durability > requiredDurability}).map(item => {return item.name});
 }
 
-function getTotalDamage( array ) {
-    let attackSum = 0;
-    array.map( function ( item ) {
-        attackSum += item.attack;
-    } );
-    return attackSum;
+getTotalDamage = () => {
+    return weapons.reduce((previousValue, currentValue) => {return previousValue + currentValue.attack}, 0);
 }
 
-function getValuestCountToSumValues( array, sum ) {
-    let innerSum = 0;
-    array.map( function ( item ) {
-        innerSum += item;
-    } );
-    if ( innerSum < sum ) {
-        return array.length + 1;
-    } else {
-        return "какое замечательное задание и преподаватели интересные =)";
-    }
-}
+getValuestCountToSumValues = (arr, sum) => {
+    return arr.reduce((previousValue, currentValue) => {
+        if (previousValue.sum < sum) {
+            previousValue.counter ++;
+            previousValue.sum += currentValue;
+            return previousValue
+        } else { return previousValue }
+    }, { sum: 0, counter: 0}).counter}
--- 3.2-array-proto-hocs/task.js
+++ 3.2-array-proto-hocs/task.js
@@ -1,20 +1,19 @@
-function sleep(milliseconds)
-{
+sleep = (milliseconds) => {
     let e = new Date().getTime() + milliseconds;
     while (new Date().getTime() <= e) {}
 }
 
-function sum(...args) {
+sum = (...args) => {
     sleep(100);
     return args.reduce((sum, arg) => {
         return sum += +arg;
     }, 0);
 }
 
-function compareArrays(arr1, arr2) {
+compareArrays = (arr1, arr2) => {
     return arr1.length === arr2.length && arr1.every(function(value, index) { return value === arr2[index]});
 }
-
+/*
 const memorize = (fn,limit) => {
     const results = {};
     return (...args) => {
@@ -28,3 +27,27 @@ const memorize = (fn,limit) => {
         return results[argsKey];
     };
 };
+ */
+
+memorize = (func, limit) => {
+    const memoryArray = [];
+    return (...arguments) => {
+        const solutionObjectFromArray = memoryArray.find(item => {return compareArrays(item.args, arguments)});
+        if (solutionObjectFromArray === undefined) {
+            const computedResult = func(...arguments);
+            memoryArray.push(
+                {
+                    args: arguments,
+                    result: computedResult
+                }
+            );
+            if (memoryArray.length > limit) {
+                memoryArray.splice(0, 1);
+            };
+            return computedResult;
+        }
+        return solutionObjectFromArray.result;
+    }
+}
+
+
